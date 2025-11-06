13731112bbaeb1e6582fd45c07cfa5c33bc7aa84
wc082025-cyber <wc082025@gmail.com>
2025-11-06 11:03:11 +0100

    added  PSApi and some other

--- PlayerScoreService.Tests/Specification.cs
+++ PlayerScoreService.Tests/Specification.cs
@@ -1,4 +1,6 @@
-﻿namespace PlayerScoreService.Tests;
+﻿using System.ComponentModel.Design;
+
+namespace PlayerScoreService.Tests;
 
 public class UnitTest1
 {
@@ -11,6 +13,37 @@ public class UnitTest1
         // Act
         var result = service.GetScore("Alice");
         // Assert
-        Assert.Null (result);
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Service_ShouldReturnCorrectScore_ForKnownPlayer()
+    {
+        //Arrange
+        var service = new PlayerScoreService();
+
+        // Act
+
+        service.Add("Alice", 5);
+        var result = service.GetScore("Alice");
+
+        // Assert
+        Assert.Equal(5, result);
+
+    }
+    [Fact]
+
+    public void Service_ShouldSumScores_Correctly()
+    {
+        // Arrange
+        var service = new PlayerScoreService();
+
+        // Act
+        service.Add("Alice", 5);
+        service.Add("Alice", 5);
+        var result = service.GetScore("Alice");
+
+        // Assert
+        Assert.Equal(10, result);
     }
 }
--- PlayerScoreService/PlayerScoreService.cs
+++ PlayerScoreService/PlayerScoreService.cs
@@ -1,9 +1,45 @@
-﻿namespace PlayerScoreService;
+﻿using System.Diagnostics.SymbolStore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PlayerScoreService;
 
 public class PlayerScoreService
 {
+    Dictionary<string, int> _score;
+
+    public PlayerScoreService()
+    {
+        _score = new Dictionary<string, int>();
+    }
     public int? GetScore(string playerAlias)
     {
-        return 1;
+        var isRegistered = _score.TryGetValue(playerAlias, out int result);
+        if (isRegistered)
+        {
+            return result;
+        }
+        else
+        {
+            return null;
+        }
     }
+
+    public void Add(string playerAlias, int score)
+    {
+        var isRegistered = _score.TryGetValue(playerAlias, out int oldScore);
+        {
+            if (isRegistered)
+            {
+                _score(playerAlias) += score;
+            }
+            else
+            {
+                _score.Add(playerAlias, score);
+            }
+        }
+
+        
+    }
+
+
 }
